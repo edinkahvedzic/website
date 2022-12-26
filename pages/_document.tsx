d58c9cb011906d9d1a2f981dba1d35e1abb9fcc0
Edin Kahvedžić <mail@edinkahvedzic.com>
2022-12-26 20:12:00 +0100

    feat: first version of application

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,47 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="./favicon.png" />
+        <meta charSet="utf-8" />
+        <meta
+          name="description"
+          content="Hello, I'm Edin Kahvedžić - Web &amp; mobile developer from Bosnia and Herzegovina."
+        />
+        <meta
+          name="keywords"
+          content="edin, kahvedzic, kahvedžić, web developer, mobile developer, bosnia, bih, visoko"
+        />
+        <meta property="og:url" content="https://edinkahvedzic.com" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="Edin Kahvedžić - Web & mobile developer"
+        />
+        <meta
+          property="og:description"
+          content="Hello, I'm Edin Kahvedžić - Web &amp; mobile developer from Bosnia and Herzegovina."
+        />
+        <meta property="og:image" content="/meta-image.jpeg" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="edinkahvedzic.com" />
+        <meta property="twitter:url" content="https://edinkahvedzic.com" />
+        <meta
+          name="twitter:title"
+          content="Edin Kahvedžić - Web & mobile developer"
+        />
+        <meta
+          name="twitter:description"
+          content="Hello, I'm Edin Kahvedžić - Web &amp; mobile developer from Bosnia and Herzegovina."
+        />
+        <meta name="twitter:image" content="./meta-image.jpeg" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&amp;display=swap"
+          rel="stylesheet"
+        />
+        <title>Edin Kahvedžić - Web & mobile developer!</title>
+        <link rel="icon" href="/favicon.png" />
       </Head>
       <body>
         <Main />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,27 @@
-export default function Home() {
-  return <main>Edin Kahvedžić</main>;
+import { landingNavigationList } from '@edinkahvedzic/website/navigation';
+export default function Index() {
+  return (
+    <div className="landing">
+      <div className="header">
+        <div className="title-wrapper">
+          <span className="cover" />
+          <h1 className="title">
+            Edin Kahvedžić
+            <span className="dot" />
+          </h1>
+        </div>
+        <div className="description-wrapper">
+          <span className="cover" />
+          <p className="description">Web & mobile developer</p>
+        </div>
+      </div>
+      <ul className="list">
+        {landingNavigationList.map((item) => (
+          <li key={item.id} className="item">
+            <a href={item.href}>{item.icon}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
